7c8e799a53d4679f294819cf979eec2184bcab0a
Chriss Quartz <chrissquartz@gmail.com>
2025-08-08 14:48:49 +1000

    Add Footer back

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Terms from './pages/terms.jsx';
 import Navbar from './components/Navbar.jsx';
+import Footer from './components/Footer.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,5 +16,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/terms" element={<Terms />} />
       </Routes>
     </BrowserRouter>
+    <Footer />
   </React.StrictMode>
 )
